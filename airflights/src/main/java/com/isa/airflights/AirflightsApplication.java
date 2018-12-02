c4c87da42c761d3639ec0a9f30ac74e18d12bc3e
SimicSvetislav <simicsvetislav@gmail.com>
2018-12-02 21:06:16 +0100

    Config

---
+++ airflights/src/main/java/com/isa/airflights/AirflightsApplication.java
@@ -0,0 +1,13 @@
+package com.isa.airflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirflightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirflightsApplication.class, args);
+		System.out.println("RADI!");
+	}
+}
---
+++ airflights/src/main/java/com/isa/airflights/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.isa.airflights.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HomeController {
+ 
+	@RequestMapping("/api/hello")
+	public String hello() {
+		return "<title>Home</title><b>Hello from HomeController<b>!";
+	}
+	
+	@RequestMapping("/api/cookie")
+	public String cookie() {
+		System.out.println("Working Directory = " +
+	              System.getProperty("user.dir"));
+		return "<title>Cookie</title><body>It's nice you stopped by, here's cookie for you.<br/> <img src='\\src\\main\\resources\\static\\img\\cookie.jpg' alt='Img error'><body><html>";
+	}
+	
+	@RequestMapping(value="/api/home", produces=MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String home() {
+		return "app.component.html";
+	}
+}
