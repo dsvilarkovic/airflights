b99838f98d0c9a8d707aadb956a79205922c3909
dsvilarkovic <tviksi96@gmail.com>
2019-01-26 19:26:13 +0100

    Konacni pokusaj za rad travis-a

---
+++ airflights/src/main/java/com/isa/airflights/model/SegmentConfig.java
@@ -0,0 +1,11 @@
+package com.isa.airflights.model;
+
+import java.util.Set;
+
+public class SegmentConfig {
+	private Set<Seat> seats;
+	
+	private Integer segmentRows;
+	private Integer segmentColumns;
+
+}
