ac1a72051dec8727ffa52fa9e685dd70f47c7ba8
djuka10 <viktordjuka10@gmail.com>
2019-01-27 19:52:59 +0100

    Testiranje master grane

--- airflights/src/main/java/com/isa/airflights/AirflightsApplication.java
+++ airflights/src/main/java/com/isa/airflights/AirflightsApplication.java
@@ -3,6 +3,11 @@ package com.isa.airflights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Glavna klasa koja pokrece aplikaciju
+ * 
+ * */
+
 @SpringBootApplication
 public class AirflightsApplication {
 
