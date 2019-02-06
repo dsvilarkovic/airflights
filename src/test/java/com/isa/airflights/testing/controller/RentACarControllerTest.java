bfcd40cb48ab4b751e2cf8adc337faaf1a48fdf5
dsvilarkovic <tviksi96@gmail.com>
2019-02-06 16:07:39 +0100

    hrk hrk

--- src/test/java/com/isa/airflights/testing/controller/RentACarControllerTest.java
+++ src/test/java/com/isa/airflights/testing/controller/RentACarControllerTest.java
@@ -1,5 +1,10 @@
 package com.isa.airflights.testing.controller;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,5 +12,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RentACarControllerTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
 
 }
