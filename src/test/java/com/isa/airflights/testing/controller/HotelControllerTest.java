a8add005e4c02567a1e9a885b293291ff43d0224
SimicSvetislav <simicsvetislav@gmail.com>
2019-02-08 18:40:35 +0100

    Testovi

--- src/test/java/com/isa/airflights/testing/controller/HotelControllerTest.java
+++ src/test/java/com/isa/airflights/testing/controller/HotelControllerTest.java
@@ -2,8 +2,22 @@ package com.isa.airflights.testing.controller;
 
 import java.nio.charset.Charset;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HotelControllerTest {
 	
 	private static final String URL_PREFIX = "/api/hotel";
@@ -11,5 +25,31 @@ public class HotelControllerTest {
 	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testGet() {
+		
+	}
 	
 }
